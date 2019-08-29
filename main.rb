4687a70ed0af3e9c7312c7344b2b431308b5cab9
Brett Bonnet <brettbonnet4@gmail.com>
2019-08-29 12:32:51 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,21 +1,23 @@
 require './linked_list.rb'
 require './node.rb'
 
-
 list = LinkedList.new
 first = list.append("number 1")
 second = list.append("number 2")
 third = list.append("number 3")
 list.to_s
 list.prepend("new number 1")
-puts list.size
+list.to_s
+list.size
 puts list.head.value
 puts list.tail.value
-puts list.at(2).value
+puts list.at(2)
 list.pop
 list.to_s
 list.contains?("number 2")
 list.find("number 3")
-list.insert_at(2, "NEW number 3")
+list.insert_at(1, "NEW number 3")
 list.to_s
-list.remove_at(2)
+list.remove_at(1)
+
+
